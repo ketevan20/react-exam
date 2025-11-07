e51fdbbdeb727392069c3fbd851edaa63fa33fe9
ketevan20 <ketevansamukashvili@gmail.com>
2025-11-07 16:36:48 +0400

    Add design

--- project/src/components/Form.jsx
+++ project/src/components/Form.jsx
@@ -1,81 +1,116 @@
 import React, { useState } from 'react'
 
 const Form = (props) => {
-    const [name, setName] = useState('');
-    const [cardNumber, setCardNumber] = useState('');
-    const [month, setMonth] = useState('');
-    const [year, setYear] = useState('');
-    const [cvc, setCvc] = useState('');
-
-    const cardNumberRegex = /^[0-9]{16}$/;
+    const [errors, setErrors] = useState({});
+    const cardNumberRegex = /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/;
     const cvcRegex = /^[0-9]{3}$/;
+    const monthRegex = /^(0[1-9]|1[0-2])$/;
+    const yearRegex = /^[0-9]{2}$/;
+
+    function Validate() {
+        const newErrors = {};
+
+        if (!props.name.trim()) {
+            newErrors.name = "Can't be blank";
+        }
+
+        if (!props.cardNumber.trim()) {
+            newErrors.cardNumber = "Can't be blank";
+        } else if (!cardNumberRegex.test(props.cardNumber)) {
+            newErrors.cardNumber = "Wrong format, numbers only";
+        }
+
+        if (!props.month.trim()) {
+            newErrors.date = "Can't be blank";
+            newErrors.month = "invalid";
+        } else if (!monthRegex.test(props.month)) {
+            newErrors.date = "Wrong date format";
+            newErrors.month = "invalid";
+        }
+
+        if (!props.year.trim()) {
+            newErrors.date = "Can't be blank";
+            newErrors.year = "invalid";
+        } else if (!yearRegex.test(props.year)) {
+            newErrors.date = "Wrong date format";
+            newErrors.year = "invalid";
+        }
+
+        if (!props.cvc.trim()) {
+            newErrors.cvc = "Can't be blank";
+        } else if (!cvcRegex.test(props.cvc)) {
+            newErrors.cvc = "Wrong format";
+        }
+
+        setErrors(newErrors);
+
+        return Object.keys(newErrors).length === 0;
+    }
+
 
 
     return (
-        <div style={{width: '50%'}}>
+        <div className='form'>
             <form className='form-container' onSubmit={(e) => {
                 e.preventDefault();
-                if(name.trim() && cardNumber.trim() && year.trim() && month.trim() && cvc.trim() 
-                    && cardNumberRegex.test(cardNumber) && cvcRegex.test(cvc)) {
-                        props.handleSubmit(name, cardNumber, year, month, cvc);
-                } else {
-                    alert('invalid input');
+                if (Validate()) {
+                    props.handleSubmit();
                 }
             }}>
                 <div>
-                    <label>Cardholder Name</label><br/>
+                    <label>Cardholder Name</label><br />
                     <input
+                        className={errors.name ? 'invalid-input' : ''}
                         type='text'
                         placeholder='e.g. Jane Appleseed'
                         onChange={(e) => {
-                            setName(e.target.value);
                             props.setName(e.target.value.trim());
-                        }} />
+                        }} /><br />
+                    {errors.name ? <small>{errors.name}</small> : ''}
                 </div>
                 <div>
-                    <label>Card Number</label><br/>
+                    <label>Card Number</label><br />
                     <input
+                        className={errors.cardNumber ? 'invalid-input' : ''}
                         type='text'
                         placeholder='e.g. 1234 5678 9123 0000'
                         onChange={(e) => {
-                            setCardNumber(e.target.value);
                             props.setCardNumber(e.target.value.trim());
-                        }} />
+                        }} /><br />
+                    {errors.cardNumber ? <small>{errors.cardNumber}</small> : ''}
                 </div>
-                <div style={{display: 'flex', gap: '1rem'}}>
-                    <div>
-                        <label>Exp. Date (MM/YY)</label><br/>
-                        <div style={{display: 'flex', gap: '1rem'}}>
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                    <div className='date-container'>
+                        <label>Exp. Date (MM/YY)</label><br />
+                        <div style={{ display: 'flex', gap: '1rem' }}>
                             <input
+                                className={errors.month ? 'invalid-input' : ''}
                                 type='number'
-                                min="1" 
-                                max="12"
                                 placeholder='MM'
                                 onChange={(e) => {
-                                    setMonth(e.target.value);
                                     props.setMonth(e.target.value.trim());
                                 }} />
                             <input
-                                min="2025" 
-                                max="3000" 
-                                step="1"
+                                className={errors.year ? 'invalid-input' : ''}
+                                min={25}
                                 type='number'
                                 placeholder='YY'
                                 onChange={(e) => {
-                                    setYear(e.target.value);
                                     props.setYear(e.target.value.trim());
                                 }} />
                         </div>
+                        {errors.date ? <small>{errors.date}</small> : ''}
                     </div>
-                    <div>
-                        <label>CVC</label><br/>
+                    <div className='cvc-container'>
+                        <label>CVC</label><br />
                         <input
+                            className={errors.cvc ? 'invalid-input' : ''}
                             type='number'
                             placeholder='e.g. 123'
                             onChange={(e) => {
-                                setCvc(e.target.value);
                                 props.setCvc(e.target.value.trim());
-                            }} />
+                            }} /><br />
+                        {errors.cvc ? <small>{errors.cvc}</small> : ''}
                     </div>
                 </div>
                 <button type='submit'>Confirm</button>
